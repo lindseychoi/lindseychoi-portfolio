844d09956efa8ca05b76b96045303c8ba2815945
Lindsey Choi <lindsey.e.choi@gmail.com>
2022-05-17 21:34:12 -0600

    getting the react build started

---
+++ src/components/Portfolio.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import NavBar from './NavBar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+
+export default function Portfolio() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Projects') {
+      return <Projects />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavBar currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
---
+++ src/components/pages/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Blog() {
+  return (
+    <div>
+      <h1>PROJECT Page</h1>
+      <p>
+        My Projects
+      </p>
+    </div>
+  );
+}
