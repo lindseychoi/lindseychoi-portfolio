3567dabcd4bf7b908c0119f9f28e18954893b7b1
Lindsey Choi <lindsey.e.choi@gmail.com>
2022-05-21 22:57:05 -0600

    imported google fonts for the entire portfolio, into the css

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,13 +7,13 @@ export default function Home() {
     <div>
       <div className="container">
 
-        <div className="row g-2 mt-0 pt-2">
+        <div className="row g-3 mt-0 pt-3">
           <div>
             <img src={ProfilePic} className="profilepic" alt="picture of me" />
           </div>
         </div>
 
-        <div className="row g-2 mt-0 pt-2">
+        <div className="row g-3 mt-0 pt-3">
           <div className="welcomeHome">
             <p>
               Welcome to my portfolio! My name is Lindsey Choi, and I'm a full-stack developer in Colorado Springs. Take a look at my projects and skills, and feel free to contact me for more information!
