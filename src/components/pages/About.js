cad42139e704feb9f29a64b1de2a66c697a9790a
Lindsey Choi <lindsey.e.choi@gmail.com>
2022-05-31 23:12:47 -0600

    working on resume button functionality

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,12 +1,12 @@
 import { faCss3, faGithub, faGitSquare, faHtml5, faJs, faNodeJs, faReact } from '@fortawesome/free-brands-svg-icons';
 import React from 'react';
-import picProfile from '../../assets/cropped-picprofile-bw.JPEG'
+import picProfile from '../../assets/cropped-picprofile-bw.JPEG';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Pdf from '../../assets/Lindsey-Choi-Resume.pdf';
 
 export default function About() {
     return (
         <div>
-
 
             <div className="container">
 
@@ -15,7 +15,7 @@ export default function About() {
                     <div className="col-md-8 pt-4">
                         <div className="card">
                             <div className="card-body">
-                            <h5 className="card-title text-center">Summary</h5>
+                                <h5 className="card-title text-center">Summary</h5>
                                 <div className="card-text">
                                     I am an aspiring developer currently enrolled in the DU Programming Bootcamp. I am excited to
                                     learn new skills
@@ -30,56 +30,28 @@ export default function About() {
                             </div>
                         </div>
 
-                        <div className="row row-cols-1">
-                            <div className="col-md-4 pt-4">
-                                <div className="card h-100 card-padding">
-                                    <div className="card-body">
-                                        <h5 className="card-title text-center">Front End</h5>
-                                        <div className="card-text">
-                                            HTML 5, CSS 3, JavaScript, React, MVC, Webpack, JSX
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="col-md-4 pt-4">
-                                <div className="card h-100 card-padding">
-                                    <div className="card-body">
-                                        <h5 className="card-title text-center">Back End</h5>
-                                        <div className="card-text">
-                                            NodeJS, JavaScript, MySQL, JSON, OOP, MongoDB, Mongoose, MERN Stack, Express, CRUD, GraphQL, Apollo, RESTful web services, API's, MySQL
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="col-md-4 pt-4">
-                                <div className="card h-100 card-padding">
-                                    <div className="card-body d-flex flex-column">
-                                        <h5 className="card-title text-center">Technologies</h5>
-                                        <div className="card-text">
-                                            Git, GitHub, Insomnia, MongoDB Compass, Sandbox, Heroku, Lighthouse, VS Code
-                                        </div>
+                        <div className="col-md-12 pt-4">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title text-center">Education</h5>
+                                    <div className="card-text">
+                                        <p className="text-center">University of Denver: Full-Stack Coding Bootcamp Certificate</p>
+                                        <p className="text-center">Colorado State University: Bachelor of Arts in English Literature</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
 
-                        {/* <div className="row row-cols-1">
-                            <div className="col-md-12 pt-4">
-                                <h5 className="text-center bigger-bolder">
-                                <div className="col-md-12 pt-4 text-center">
-                                    <a className="fa-2xl fa-cog fa-flag fa-brands logo-padding-2"><FontAwesomeIcon icon={faHtml5} /></a>
-                                    <a className="fa-2xl fa-cog fa-flag fa-brands logo-padding-2"><FontAwesomeIcon icon={faCss3} /></a>
-                                    <a className="fa-2xl fa-cog fa-flag fa-brands logo-padding-2"><FontAwesomeIcon icon={faJs} /></a>
-                                    <a className="fa-2xl fa-cog fa-flag fa-brands logo-padding-2"><FontAwesomeIcon icon={faReact} /></a>
-                                    <a className="fa-2xl fa-cog fa-flag fa-brands logo-padding-2"><FontAwesomeIcon icon={faNodeJs} /></a>
-                                    <a className="fa-2xl fa-cog fa-flag fa-brands logo-padding-2"><FontAwesomeIcon icon={faGithub} /></a>
-                                    <a className="fa-2xl fa-cog fa-flag fa-brands logo-padding-2"><FontAwesomeIcon icon={faGitSquare} /></a>
+                        <div className="col-md-12 pt-4">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title text-center">Skills</h5>
+                                    <div className="card-text text-center">
+                                        <p>Verbal and written communication, presenting and instructing, coaching, team player, adapts quickly</p>
+                                    </div>
                                 </div>
-                                </h5>
                             </div>
-                        </div> */}
+                        </div>
 
                     </div>
 
@@ -87,28 +59,76 @@ export default function About() {
                         <img src={picProfile} className="aboutPic" alt="me on a mountain" />
                     </div>
 
-                    <div className="col-md-11 pt-4">
                     <div className="row row-cols-1">
-                            <div className="col-md-12 pt-4">
-                                <h5 className="text-center bigger-bolder">
-                                <div className="col-md-12 pt-4 text-center">
-                                    <a className="fa-2xl fa-cog fa-flag fa-brands logo-padding-2"><FontAwesomeIcon icon={faHtml5} /></a>
-                                    <a className="fa-2xl fa-cog fa-flag fa-brands logo-padding-2"><FontAwesomeIcon icon={faCss3} /></a>
-                                    <a className="fa-2xl fa-cog fa-flag fa-brands logo-padding-2"><FontAwesomeIcon icon={faJs} /></a>
-                                    <a className="fa-2xl fa-cog fa-flag fa-brands logo-padding-2"><FontAwesomeIcon icon={faReact} /></a>
-                                    <a className="fa-2xl fa-cog fa-flag fa-brands logo-padding-2"><FontAwesomeIcon icon={faNodeJs} /></a>
-                                    <a className="fa-2xl fa-cog fa-flag fa-brands logo-padding-2"><FontAwesomeIcon icon={faGithub} /></a>
-                                    <a className="fa-2xl fa-cog fa-flag fa-brands logo-padding-2"><FontAwesomeIcon icon={faGitSquare} /></a>
+
+                        <div className="col-md-4 pt-4">
+                            <div className="card h-100 card-padding">
+                                <div className="card-body d-flex flex-column">
+                                    <h5 className="card-title text-center">Technologies</h5>
+                                    <div className="card-text">
+                                        Mongoose, Express, MySQL, MongoDB, MySQL, NodeJS, Git, GitHub, Insomnia, MongoDB Compass, Sandbox, Heroku, Lighthouse, VS Code, MERN Stack, GraphQL, Apollo
+                                    </div>
                                 </div>
-                                </h5>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4 pt-4">
+                            <div className="card h-100 card-padding">
+                                <div className="card-body">
+                                    <h5 className="card-title text-center">Programming Languages</h5>
+                                    <div className="card-text">
+                                        HTML 5, CSS 3, JavaScript, React, MVC, Webpack, JSX
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4 pt-4">
+                            <div className="card h-100 card-padding">
+                                <div className="card-body">
+                                    <h5 className="card-title text-center">Technical Concepts and Data</h5>
+                                    <div className="card-text">
+                                        JSON, OOP, CRUD, RESTful web services, API, Progressive Web Applications
+                                    </div>
+                                </div>
                             </div>
                         </div>
 
                     </div>
 
 
+                    <div className="row row-cols-1">
+                        <div className="col-md-12 pt-4 text-center resume">
+                            <button className=" btn btn-success btn-block shadow-none" href = {Pdf} target = "_blank">Download a PDF of my Resumé</button>
+                        </div>
+                    </div>
+
+
+                    <div className="row row-cols-1">
+                        <div className="col-md-11 pt-4">
+                            <div className="row row-cols-1">
+                                <div className="col-md-12 pt-4">
+                                    <h5 className="text-center bigger-bolder">
+                                        <div className="col-md-12 pt-4 text-center">
+                                            
+                                            <a className="fa-2xl fa-cog fa-flag fa-brands logo-padding-2"><FontAwesomeIcon icon={faHtml5} /></a>
+                                            <a className="fa-2xl fa-cog fa-flag fa-brands logo-padding-2"><FontAwesomeIcon icon={faCss3} /></a>
+                                            <a className="fa-2xl fa-cog fa-flag fa-brands logo-padding-2"><FontAwesomeIcon icon={faJs} /></a>
+                                            <a className="fa-2xl fa-cog fa-flag fa-brands logo-padding-2"><FontAwesomeIcon icon={faReact} /></a>
+                                            <a className="fa-2xl fa-cog fa-flag fa-brands logo-padding-2"><FontAwesomeIcon icon={faNodeJs} /></a>
+                                            <a className="fa-2xl fa-cog fa-flag fa-brands logo-padding-2"><FontAwesomeIcon icon={faGithub} /></a>
+                                            <a className="fa-2xl fa-cog fa-flag fa-brands logo-padding-2"><FontAwesomeIcon icon={faGitSquare} /></a>
+                                        </div>
+                                    </h5>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
+
     );
 }
